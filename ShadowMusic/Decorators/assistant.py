5a831eeb3f64d7a3e21f39a7b2b5ed82094116a6
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-12-19 16:36:04 +0530

    Shadow Music

--- ShadowMusic/Decorators/assistant.py
+++ ShadowMusic/Decorators/assistant.py
@@ -1,25 +1,37 @@
-import asyncio
-
+import random
 from typing import Dict, List, Union
 
 from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
 
-from ShadowMusic import (ASSID, ASSMENTION, ASSNAME, ASSUSERNAME, BOT_ID, app,
-                   userbot)
+from ShadowMusic import MUSIC_BOT_NAME, app
+from ShadowMusic.Database import get_assistant, save_assistant
+from ShadowMusic.Utilities.assistant import get_assistant_details, random_assistant
 
 
 def AssistantAdd(mystic):
     async def wrapper(_, message):
+        _assistant = await get_assistant(message.chat.id, "assistant")
+        if not _assistant:
+            ran_ass = random.choice(random_assistant)
+            assis = {
+                "saveassistant": ran_ass,
+            }
+            await save_assistant(message.chat.id, "assistant", assis)
+        else:
+            ran_ass = _assistant["saveassistant"]
+        ASS_ID, ASS_NAME, ASS_USERNAME, ASS_ACC = await get_assistant_details(
+            ran_ass
+        )
         try:
-            b = await app.get_chat_member(message.chat.id, ASSID)
+            b = await app.get_chat_member(message.chat.id, ASS_ID)
             if b.status == "kicked":
                 return await message.reply_text(
-                    f"Assistant Account[{ASSID}] is banned.\nUnban it first to use Music Bot\n\nUsername: @{ASSUSERNAME}"
+                    f"Assistant Account [{ASS_ID}] is banned.\nUnban it first to use Music Bot\n\nUsername: @{ASS_USERNAME}"
                 )
         except UserNotParticipant:
             if message.chat.username:
                 try:
-                    await userbot.join_chat(message.chat.username)
+                    await ASS_ACC.join_chat(message.chat.username)
                 except UserAlreadyParticipant:
                     pass
                 except Exception as e:
@@ -33,7 +45,7 @@ def AssistantAdd(mystic):
                     inv_lnk = str(xxy)
                     inv_lnk = inv_lnk.replace("https://t.me/+","https://t.me/joinchat/")
                     await asyncio.sleep(10)
-                    await userbot.join_chat(inv_lnk)
+                    await ASS_ACC.join_chat(inv_lnk)
                     await message.reply(f"{ASSNAME} Joined Successfully") 
                 except UserAlreadyParticipant:
                     pass
