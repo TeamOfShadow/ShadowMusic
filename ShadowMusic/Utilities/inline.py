bd054e966997178dd18ec7b7eda3d97b4790302e
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2022-01-01 09:55:58 +0530

    Shadow Music

--- ShadowMusic/Utilities/inline.py
+++ ShadowMusic/Utilities/inline.py
@@ -69,15 +69,15 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
         ] + [
             (
                 EqInlineKeyboardButton(
-                    "❮",
+                    "⬅️",
                     callback_data="{}_prev({})".format(prefix, modulo_page),
                 ),
                 EqInlineKeyboardButton(
-                    "Close",
+                    "🗑 Close",
                     callback_data="close",
                 ),
                 EqInlineKeyboardButton(
-                    "❯",
+                    "⬅️",
                     callback_data="{}_next({})".format(prefix, modulo_page),
                 ),
             )
