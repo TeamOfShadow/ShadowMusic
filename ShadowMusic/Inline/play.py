6570940285ec81d7b955f34f586a4996dbc02d42
SithumDimalsha4 <97735163+SithumDimalsha4@users.noreply.github.com>
2022-04-30 06:25:31 +0530

    Update play.py

--- ShadowMusic/Inline/play.py
+++ ShadowMusic/Inline/play.py
@@ -7,13 +7,13 @@ from ShadowMusic import db_mem
 def url_markup(videoid, duration, user_id, query, query_type):
     buttons = [
         [
-            InlineKeyboardButton(text="⬅️", callback_data=f"slider B|{query_type}|{query}|{user_id}"),
-            InlineKeyboardButton(text="🎶 Play Now", callback_data=f"ShadowMusic {videoid}|{duration}|{user_id}"),
-            InlineKeyboardButton(text="➡️", callback_data=f"slider F|{query_type}|{query}|{user_id}"),
+            InlineKeyboardButton(text="❰❰", callback_data=f"slider B|{query_type}|{query}|{user_id}"),
+            InlineKeyboardButton(text="▶️ Play Nowv", callback_data=f"ShadowMusic {videoid}|{duration}|{user_id}"),
+            InlineKeyboardButton(text="❱❱", callback_data=f"slider F|{query_type}|{query}|{user_id}"),
         ],
         [
-            InlineKeyboardButton(text="🔎 More Results", callback_data=f"Search {query}|{user_id}"),
-            InlineKeyboardButton(text="🗑 Close Search", callback_data=f"forceclose {query}|{user_id}"),
+            InlineKeyboardButton(text="🔍 More Results", callback_data=f"Search {query}|{user_id}"),
+            InlineKeyboardButton(text="❌ Close Menu", callback_data=f"forceclose {query}|{user_id}"),
         ],
     ]
     return buttons
@@ -22,8 +22,8 @@ def url_markup(videoid, duration, user_id, query, query_type):
 def url_markup2(videoid, duration, user_id):
     buttons = [
         [
-            InlineKeyboardButton(text="🎶 Play Now", callback_data=f"ShadowMusic {videoid}|{duration}|{user_id}"),
-            InlineKeyboardButton(text="🗑 Close Search", callback_data=f"forceclose {videoid}|{user_id}"),
+            InlineKeyboardButton(text=" ▶️ Play Now", callback_data=f"ShadowMusic {videoid}|{duration}|{user_id}"),
+            InlineKeyboardButton(text="❌ Close Menu", callback_data=f"forceclose {videoid}|{user_id}"),
         ],
     ]
     return buttons
